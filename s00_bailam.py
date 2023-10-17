7cc7349baaa6138b4382a17a8e40a25a6b4290a0
minhhuyng21 <142973912+minhhuyng21@users.noreply.github.com>
2023-10-17 07:37:22 +0000

    bai nop

--- s00_bailam.py
+++ s00_bailam.py
@@ -47,8 +47,46 @@ greeting('2100')             | Good evening!          | 14
 
 """
 #endregion debai
-
-#region bailam
 def greeting(hour_str):
-  return 'todo'
+  hour_str = hour_str.lower()
+  import re
+  t = re.findall("[A-Za-z+]",hour_str)
+  k = re.findall("\d+",hour_str)
+  k1 = k[0]
+  if (len(k[0]) == 4) and (t == []):
+    k1 = int(k1)
+    if (k1 >= 600) and (k1 <= 1200):
+       return "Good morning!"
+    return "Good evening!"
+
+  if ":" in hour_str:
+    if "pm" in hour_str: 
+      hour_str = hour_str.split(":")
+      a = int(hour_str[0])
+      if (a > 1):
+        return "Good evening!"  
+      else:
+        return "Good afternoon! "
+    elif "am" in hour_str:
+      return "Good morning!"
+    else:
+      hour_str = hour_str.split(":")
+      a = int(hour_str[0])
+      if (a>=6) and (a<=12):
+        return "Good morning!"
+      else:
+        return "Good evening!"  
+    
+  if "am" in hour_str:
+    m = re.findall("\d+",hour_str)
+    return "Good morning!"
+  elif "pm" in hour_str:
+    m = re.findall("\d+",hour_str)
+    m1 = int(m[0]) 
+    if (m1>1):
+      return "Good evening!"
+    else:
+      return "Good afternoon!"
+#region bailam
+
 #endregion bailam
